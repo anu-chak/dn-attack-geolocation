e975a506e943c0d2e3b36c5116e0272f14e4427c
anu-chak <36936724+anu-chak@users.noreply.github.com>
2018-02-28 19:17:33 -0800

    Update 1_Tcpd_to_DB.py

--- 1_Tcpd_to_DB.py
+++ 1_Tcpd_to_DB.py
@@ -1,111 +1,176 @@
 #!/usr/bin/python
 
+import re
+import collections
 import sqlite3
-import webbrowser 
-import math
-
-class maps:
-
-	def __init__(self, centerLat, centerLng, zoom ):
-		self.center = (float(centerLat),float(centerLng))
-		self.zoom = int(zoom)
-		self.paths = []
-		self.clientlat = None
-		self.clientlng= None
-		self.destlat = None
-		self.destlng=None
 		
-	def addclientpoint(self, lat, lng):
-		self.clientlat= lat
-		self.clientlng= lng
+def time(match):
+	patt = re.compile("(\d{2}[:]\d{2}[:]\d{2}[.]\d{6})")
+	match = patt.search(f)
+	if match:
+		x=match.group()
+		return x
 		
-	def adddestpoint(self, lat, lng):
-		self.destlat = lat
-		self.destlng= lng
+def protocol(match):
+	patt = re.compile("(IP)")
+	match = patt.search(f)
+	if match:
+		x=match.group()
+		return x
 		
-	#create the html file which include one google map and all points and paths
-	def draw(self, htmlfile):
-		f = open(htmlfile,'wb')
-		f.write('<html>\n')
-		f.write('<head>\n')
-		f.write('<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />\n')
-		f.write('<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>\n')
-		f.write('<title>Google Maps - pygmaps </title>\n')
-		f.write('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>\n')
-		f.write('<script type="text/javascript">\n')
-		f.write('\tfunction initialize() {\n')
-		self.drawmap(f)
-		self.drawpoints(f)
-		#self.drawpaths(f,self.paths)
-		f.write('\t}\n')
-		f.write('</script>\n')
-		f.write('</head>\n')
-		f.write('<body style="margin:0px; padding:0px;" onload="window.settimeout(initialize(),1000)">\n')
-		f.write('\t<div id="map_canvas" style="width: 100%; height: 100%;"></div>\n')
-		f.write('</body>\n')
-		f.write('</html>\n')		
-		f.close()
+def clientport(cip):
+	patt= re.compile("(\\.([0-9]+) >)")
+	m= patt.search(cip)
+	if m:
+		cprt=m.group(2)
+	else:
+		cprt= "#"
+	return cprt
+def clientip1(match):
+	patt = re.compile("(([a-zA-Z0-9.-]+) >)")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+		return x
+def clientip2(match):
+	patt = re.compile("(([a-zA-Z0-9.-]+)\\.[0-9]+ >)")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+		return x
+		
+def destport(match):
+	patt = re.compile("(\\.([0-9]+)[:])")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+	else:
+	    x="#"
+	return x
+def destip1(match):
+	patt = re.compile("(> ([A-Za-z0-9.\-_]+)[:])")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+		return x
+def destip2(match):
+	patt = re.compile("(> ([A-Za-z0-9.\-_]+)(\\.([0-9]+)[:]))")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+		return x
+		
+def dnsidnum(match):
+	patt = re.compile("(: ([0-9]+)(\\+|\\*|\\-| ))")
 	
-	def drawpoints(self,f):
-		conn = sqlite3.connect('DNSLog.db')
-		print "Opened database successfully";
-		rs=conn.execute('''SELECT * FROM ATTACK_LOC''')
-		resultset=rs.fetchall()
-		
-		f.write('\t\tvar latlng;\n')
-		f.write('\t\tvar markerlist=[];\n')
-		f.write('\t\tvar markers=[];\n')
-		f.write('\t\tvar iterator=0;\n')
-		f.write('\t\tvar icon;\n')
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+		return x
 
-		for r in resultset:
-			print r[3]," ",r[4]," ",r[6]," ",r[7]
+def dnssymbol(match):
+	patt = re.compile("(: ([0-9]+)(\\+|\\*|\\-| ))")
+	match = patt.search(f)
+	if match:
+		x=match.group(3)
+		if x==" ":
+			x="#"
+	else:
+		x="#"
+	return x
 
-			f.write('\t\tlatlng = new google.maps.LatLng(%f, %f);\n'%(r[3],r[4]))
-			f.write('\t\tmarkerlist.push(latlng);\n')
-			f.write('\t\tlatlng = new google.maps.LatLng(%f, %f);\n'%(r[6],r[7]))
-			f.write('\t\tmarkerlist.push(latlng);\n')
+def nxdsearch(match):
+	patt = re.compile("(NXDomain|ServFail)")
+	match = patt.search(f)
+	if match:
+		x=match.group()
+		return x
 		
-		f.write('\n')
-		f.write('\t\tfor (var i = 0; i < markerlist.length; i++) {\n')
-		f.write('\t\t\tsetTimeout(function() {\n')
-		
-		f.write('\t\t\timg = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/micons/red-dot.png");\n')
-		f.write('\t\t\tmarkers.push(new google.maps.Marker({\n')
-		f.write('\t\t\ticon: img,\n')
-		f.write('\t\t\t\tposition: markerlist[iterator],\n')
-		f.write('\t\t\t\tmap: map,\n')	
-		f.write('\t\t\t\tdraggable: false,\n')
-		f.write('\t\t\t\tanimation: google.maps.Animation.DROP\n')
-		f.write('\t\t\t}));\n')
-		f.write('\t\t\titerator++;\n')
-		
-		f.write('\t\t\timg = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/green-dot.png");\n')
-		f.write('\t\t\tmarkers.push(new google.maps.Marker({\n')
-		f.write('\t\t\ticon: img,\n')
-		f.write('\t\t\t\tposition: markerlist[iterator],\n')
-		f.write('\t\t\t\tmap: map,\n')	
-		f.write('\t\t\t\tdraggable: false,\n')
-		f.write('\t\t\t\tanimation: google.maps.Animation.DROP\n')
-		f.write('\t\t\t}));\n')
-		f.write('\t\t\titerator++;\n')
-		
-		f.write('\t\t},i * 2000);\n')
-		f.write('\t}\n')
+def querytype(match):
+	patt = re.compile("(([0-9]+/[0-9]+/[0-9]+)|\+|\-|\*) ([A-Z]+(\?)?)")
+
+	match = patt.search(f)
 	
-	def drawmap(self, f):
-		f.write('\t\tvar centerlatlng = new google.maps.LatLng(%f, %f);\n' % (self.center[0],self.center[1]))
-		f.write('\t\tvar myOptions = {\n')
-		f.write('\t\t\tzoom: %d,\n' % (self.zoom))
-		f.write('\t\t\tcenter: centerlatlng,\n')
-		f.write('\t\t\tmapTypeId: google.maps.MapTypeId.ROADMAP\n')
-		f.write('\t\t};\n')
-		f.write('\t\tvar map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);\n')
-		f.write('\n')
+	if match:
+		x=match.group(3)
+	else:
+	    x= "#"
+	return x
+		
+def authrec(match):
+	patt = re.compile("([0-9]+/[0-9]+/[0-9]+)")
+	patt2= re.compile("\[[1][a][u]\]")
+	match = patt.search(f)
+	match2= patt2.search(f)
+	if match:
+		x=match.group()
+	elif match2:
+	    x=match2.group()
+	else:
+	    x="#"
+	return x
 
-if __name__ == "__main__":
+def dnsname(match):
+	patt = re.compile("(([A-Za-z0-9.\-_\?]{2,200})((\\,)|( \([0-9]+\)$)))")
+	match = patt.search(f)
+	if match:
+		x=match.group(2)
+	else:
+	    x= "#"
+	return x
+		
+conn = sqlite3.connect('DNSLog.db')
+cur= conn.cursor()
+print "Opened database successfully";
+conn.execute('''DROP TABLE IF EXISTS TCPD_OUTPUT''')
+print "Table dropped successfully";
+conn.execute('''CREATE TABLE TCPD_OUTPUT
+			   (TIME CHAR(13), 
+				PROTOCOL CHAR(3), 
+				CLIENT_IP CHAR(200), 
+				CLIENT_PORT CHAR(10), 
+				DEST_IP CHAR(200), 
+				DEST_PORT CHAR(10), 
+				DNS_ID CHAR(10), 
+				DNS_SYM CHAR(2), 
+				AUTH_REC CHAR(10), 
+				QUERY_TYPE CHAR(10), 
+				DNS_NAME CHAR(200))''')
+				
+file = open('dnslog3.txt', 'r')
+print file
+c=0
+
+for f in file:
+	f = f.strip()
+	c= c+1
 	
-	mymap = maps(37.428, -10, 2)
-	mymap.draw('mymap.draw.html') 
-	url = 'mymap.draw.html'
-	webbrowser.open_new_tab(url) 
+	timestamp= time(f)
+	proto= protocol(f)
+	
+	cprt= clientport(f)
+	if cprt=="#":
+	    cip= clientip1(f)
+	else:
+	    cip= clientip2(f)
+		
+	dprt= destport(f)
+	if dprt=="#":
+		dip= destip1(f)
+	else:
+		dip= destip2(f)
+	
+	dnsid= dnsidnum(f)
+	dnssym= dnssymbol(f)
+	rec= authrec(f)
+		
+	qtype= querytype(f)
+	dnsnm= dnsname(f)
+	
+	print c," ",timestamp," ",proto," ",cip," ",cprt," ",dip," ",dprt," ",dnsid,dnssym," ",rec," ",qtype," ",dnsnm	
+	stmt='''INSERT INTO TCPD_OUTPUT (TIME, PROTOCOL, CLIENT_IP, CLIENT_PORT, DEST_IP, DEST_PORT, 
+			DNS_ID, DNS_SYM, AUTH_REC, QUERY_TYPE, DNS_NAME) VALUES (?,?,?,?,?,?,?,?,?,?,?);'''
+	cur.execute(stmt, (timestamp, proto, cip, cprt, dip, dprt, dnsid, dnssym, rec, qtype, dnsnm))
+	conn.commit()
+print "Table created successfully";
+conn.close()
